8e3af66462f8fab704442b3205254cf64bbd710b
Acrobaticbag3 <kevintom214@gmail.com>
2022-12-04 20:58:25 +0100

    Done

--- Program.cs
+++ Program.cs
@@ -2,8 +2,40 @@
 
 namespace TryParse {
     internal class program {
-        public static void Main(String[] args) {
-            
+        public static void Main(string[] args) {
+            int result;
+            if (!int.TryParse(s: Console.ReadLine(), result: out result))
+                Console.Write(value: "Input a number "); 
+            else {
+                Console.WriteLine(result);
+            }
+        }
+
+        public static bool TryParse(string s, out int result) {
+            bool parsed;
+            int tempRes = 0;
+            result = tempRes;
+
+            try {
+
+                result = Convert.ToInt32(value: s);
+                parsed = true;
+
+                if (parsed == true) {
+                    return true;
+                }
+
+            } catch (System.Exception) {
+
+                parsed = false;
+
+                if (parsed == false) {
+                    return false;
+                }
+
+            }
+
+            return false;
         }
     }
 }
